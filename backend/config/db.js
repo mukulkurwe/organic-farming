7beb7d5596db5746091f3cfec9f31848c307e8f3
mukulkurwe <mukulkurwe18@gmail.com>
2026-02-13 15:18:00 +0530

    cloud change

--- backend/config/db.js
+++ backend/config/db.js
@@ -16,9 +16,8 @@ import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: false, // Internal Render Postgres URL works without SSL
-});
+  LconnectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },});
 
 export default pool;
 
