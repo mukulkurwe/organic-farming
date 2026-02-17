57d456981cee3a9d2f7b0a347d82ade131544450
mukulkurwe <mukulkurwe18@gmail.com>
2026-02-17 13:56:16 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -16,33 +16,10 @@ import supervisorRoutes from "./routes/supervisorRoutes.js";
 
 dotenv.config();
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://organic-farming-teal.vercel.app",
-];
-
 const app = express();
-// for local host
-// app.use(cors({ origin: "http://localhost:3000" }));
 
-// for production
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like Postman/curl)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) return callback(null, true);
-
-    return callback(new Error("Not allowed by CORS: " + origin));
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
-
-// ✅ handle preflight for all routes
-app.options("*", cors());
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(express.json());
 
 /* ========================
    ROUTES
