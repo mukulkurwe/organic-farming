ec6db12cf8055a77deaa3b1a4ffd898342603e9c
mukulkurwe <mukulkurwe18@gmail.com>
2026-02-17 14:58:32 +0530

    localdb

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ if (!process.env.DATABASE_URL) {
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  // ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
