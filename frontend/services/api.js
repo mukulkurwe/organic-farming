d6c535dd60fa0e4dcba42a09915a32c1de7b7a36
mukulkurwe <mukulkurwe18@gmail.com>
2026-02-17 15:30:27 +0530

    changes in api calling

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -11,22 +11,3 @@ const api = axios.create({
 
 export default api;
 
-
-// new code
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "http://localhost:5000/api",
-// });
-
-// api.interceptors.request.use((config) => {
-//   if (typeof window !== "undefined") {
-//     const token = localStorage.getItem("token");
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//   }
-//   return config;
-// });
-
-// export default api;
