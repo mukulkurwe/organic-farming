63f631be6933bfb8767d5069406146012cc83be6
mukulkurwe <mukulkurwe18@gmail.com>
2026-02-27 12:29:38 +0530

    production-env

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -19,14 +19,24 @@
 // for production+ local
 
 
+// import axios from "axios";
+
+// const baseURL =
+//   process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api";
+
+// const api = axios.create({
+//   baseURL,
+//   timeout: 30000,
+// });
+
+// export default api;
+
+// services/api.js
 import axios from "axios";
 
-const baseURL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api";
-
 const api = axios.create({
-  baseURL,
-  timeout: 30000,
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // ✅
+  withCredentials: true,
 });
 
 export default api;
